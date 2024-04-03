9c534f82c927976ca302cba5f2bb5c4d25c08e35
LiL-14 <lucasballonje12@gmail.com>
2024-04-03 01:12:44 -0300

    feat metodo post

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
 import fastify from "fastify";
+import { z } from "zod"
+import { PrismaClient } from "@prisma/client";
 
 const app = fastify();
+
+const prisma = new PrismaClient({
+    log: ["query"],
+})
 
 app.listen({ port: 3333}).then(() => {
     console.log("HTTP Servidor rodando");
@@ -8,10 +14,24 @@ app.listen({ port: 3333}).then(() => {
 });
 
 
-app.get("/", () => {
-    return "Funcionando"
-})
+app.post("/events", async (req, reply) => {
+    const createEventSchema = z.object({
+        title: z.string().min(4),
+        details: z.string().nullable(),
+        maximumAttendees: z.number().int().positive().nullable(),
+    })
 
-app.get("/teste", () => {
-    return "Teste"
+    const data = createEventSchema.parse(req.body);
+    
+    const event = await prisma.event.create({
+        data: {
+            title: data.title,
+            details: data.details,
+            maximumAttendees: data.maximumAttendees,
+            slug: new Date().toISOString(),
+        }
+    })
+
+    // return { eventId: event.id }
+    return reply.status(201).send({ eventId: event.id })
 })
